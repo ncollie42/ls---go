578425c60db63f424168d465933779428c673acd
Nico <ncollie42@gmail.com>
2019-07-29 22:03:41 -0700

    First day

--- main.go
+++ main.go
@@ -1,63 +1,132 @@
 package main
 
 import (
-	"io/ioutil"
-	"fmt"
 	"flag"
+	"fmt"
 	"os"
-	// "path/filepath"
+	"path/filepath"
+	"sort"
+	"syscall"
 )
-var	l_flag, r_flag, R_flag, a_flag bool
+
+var l_flag, r_flag, R_flag, a_flag bool
 var g_path string
 
 func init() {
-	flag.BoolVar(&l_flag,"l", false, "long format")
+	flag.BoolVar(&l_flag, "l", false, "long format")
 	flag.BoolVar(&a_flag, "a", false, "long format")
-	flag.StringVar(&g_path, "path", "/Users/Nico/Desktop/main", "long format")
+	flag.BoolVar(&R_flag, "R", false, "long format")
+	flag.StringVar(&g_path, "path", ".", "long format")
 	flag.Parse()
 }
 
-func test( path string, info os.FileInfo, err error) error {
-	if a_flag || info.Name()[0] != '.' {
-		if info.IsDir() {
-			fmt.Println(path)
+func readDir(dirname string) ([]os.FileInfo, error) {
+	f, err := os.Open(dirname)
+	if err != nil {
+		return nil, err
+	}
+	list, err := f.Readdir(-1)
+	f.Close()
+	if err != nil {
+		return nil, err
+	}
+	sort.Slice(list, func(i, j int) bool { return list[i].Name() < list[j].Name() }) // sort how I'd like?
+	return list, nil
+}
+
+// func readDirNames(dirname string) ([]string, error) {
+// 	f, err := os.Open(dirname)
+// 	if err != nil {
+// 		return nil, err
+// 	}
+// 	names, err := f.Readdirnames(-1)
+// 	f.Close()
+// 	if err != nil {
+// 		return nil, err
+// 	}
+// 	sort.Strings(names) // sort how ever i need			---- also remove the '.' here?
+// 	return names, nil
+// }
+
+func printDir(files []os.FileInfo) {
+	// table to fall function?
+	if l_flag {
+		for _, file := range files {
+			if a_flag || file.Name()[0] != '.' {
+				fmt.Println(file.Mode(), "links", "name1", "name2", file.Size(), file.ModTime().Format("Jan _2 15:04"), file.Name())
+			}
 		}
-		// fmt.Println(info.Name())
-		// files = append(files, path)
+	} else {
+		for _, file := range files {
+			if a_flag || file.Name()[0] != '.' {
+				fmt.Println(file.Name())
+			}
+		}
+	}
+}
+
+func walk(path string, info os.FileInfo) error {
+	stats, err := readDir(path)
+	if err != nil {
+		return err
+	}
+	printDir(stats)
+	if R_flag {
+		for _, file := range stats {
+			if a_flag || file.Name()[0] != '.' {
+				fileName := filepath.Join(path, file.Name())
+				fileInfo, err := os.Lstat(fileName)
+				if err != nil {
+					return err
+				}
+				if fileInfo.IsDir() {
+					fmt.Println(fileName, ":")
+					walk(fileName, fileInfo)
+				}
+			}
+		}
 	}
 	return nil
 }
 
+func checkInput(root string) error { //check if it's a valid file/ dir
+	info, err := os.Lstat(root)
+	if err != nil {
+		fmt.Println("Bad path? or invalid name?")
+	} else if info.IsDir() {
+		return walk(root, info)
+	} else {
+		fmt.Println("it'not a dir - simple print", root)
+	}
+	return nil
+}
+
+func test(name string) {
+	info, err := os.Lstat(name)
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(info)
+	fmt.Printf("\n%v", info)
+	fmt.Printf("\n\n%#v", info)
+	fmt.Printf("\n\n%#v", info.Sys())
+	tmp := info.Sys().(*syscall.Stat_t)
+	fmt.Printf("\n\n%v", tmp.Nlink)
+	fmt.Printf("\n\n%v", tmp.Blocks)
+	fmt.Printf("\n\n%v", tmp.Blksize)
+	//get group name from group id?
+	// s, ok := info.Sys().(*syscall.Stat_t)
+
+}
+
 func main() {
-	// var files []string
+	// parse flags like in LS
+	// pass in a list of inputs, print bads, regs, and then go into dirs
 
+	test("tmp")
 
-	// filepath.Walk(g_path , test)
-
-	fd , err := os.Open(".")
-	if (err != nil) {
-		os.Exit(0)
-	}
-
-	_, err= ioutil.ReadDir("main")
-	if err != nil {
-		fmt.Println(err)
-	}
-
-	// files. err := fd.Name/
-	tmp , err := fd.Readdirnames(-1)
-	if err != nil {
-		fmt.Println(err)
-	}
-	fmt.Println(fd.Name(), tmp)
-	// for _, file := range files {
-	// 	fmt.Println(file)
-	// }
-	// files, err := ioutil.ReadDir(".")
+	// err := checkInput(g_path)
 	// if err != nil {
-	// 	fmt.Errorf("Error reading dir")
-	// }
-	// for _, file := range files {
-	// 	fmt.Println(file.Mode(), file.Size(), file.Name(),"\n")
+	// 	println(err)
 	// }
 }
